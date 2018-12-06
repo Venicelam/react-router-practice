8ac0077d4a4d2aafc13fafd01971cb4fd30057a4
lamve2 <venice.lam@oocl.com>
2018-12-06 14:49:00 +0800

    add go back button

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
 
 export default class AboutPage extends Component {
+  onClick = () => {
+    const {history} = this.props
+    history.goBack()
+    //history.go(-1)  go back one page
+  }
   render() {
     return (
       <div>
-        About Page
+        <h2>About Page</h2>
+         <button onClick={this.onClick}>go back</button> 
       </div>
     )
   }
